ad474076f3b753b8889f76689ffba65987264e23
Eudes Andrade <eudes.andrade@gmail.com>
2015-03-16 16:09:48 -0300

    Versão inicial do job que filra posts de usuários brasileiros

---
+++ stackoverflow/run.sh
@@ -0,0 +1,2 @@
+rm -rf /home/eudes/desenvolvimento/hadoop//workspace/hadoop-lab/files/output/stackoverflow-usersfilter
+/home/eudes/desenvolvimento/hadoop/hadoop-2.6.0/bin/hadoop jar /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/stackoverflow/target/stackoverflow-0.0.1-SNAPSHOT.jar br.edu.ifba.gsort.inf628.main.BrazilianUsers /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/files/input/stackoverflow/stackoverflow.com-Users-reduced /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/files/output/stackoverflow/brazilianUsers
---
+++ word-count/run.sh
@@ -0,0 +1,2 @@
+rm -rf /home/eudes/desenvolvimento/hadoop//workspace/hadoop-lab/files/output/word-count
+/home/eudes/desenvolvimento/hadoop/hadoop-2.6.0/bin/hadoop jar /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/word-count/target/word-count-0.0.1-SNAPSHOT.jar br.edu.ifba.gsort.inf628.main.WordCount /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/files/input/word-count /home/eudes/desenvolvimento/hadoop/workspace/hadoop-lab/files/output/word-count
