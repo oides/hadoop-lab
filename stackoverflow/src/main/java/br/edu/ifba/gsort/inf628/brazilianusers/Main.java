b222ca6660b761ea04c0341a0f9b4bb7d5e7d4cf
Eudes Andrade <eudes.andrade@gmail.com>
2015-03-16 21:47:21 -0300

    Implementando filtro de usuários do brasil

--- stackoverflow/src/main/java/br/edu/ifba/gsort/inf628/brazilianusers/Main.java
+++ stackoverflow/src/main/java/br/edu/ifba/gsort/inf628/brazilianusers/Main.java
@@ -9,32 +9,18 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Main {
 
-//	public static class IntSumReducer extends
-//			Reducer<Text, IntWritable, Text, IntWritable> {
-//		private IntWritable result = new IntWritable();
-//
-//		public void reduce(Text key, Iterable<IntWritable> values,
-//				Context context) throws IOException, InterruptedException {
-//			int sum = 0;
-//			for (IntWritable val : values) {
-//				sum += val.get();
-//			}
-//			result.set(sum);
-//			context.write(key, result);
-//		}
-//	}
-
 	public static void main(String[] args) throws Exception {
+		
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "Brazilian Users");
 		job.setJarByClass(Main.class);
 		job.setMapperClass(Map.class);
-//		job.setCombinerClass(IntSumReducer.class);
-//		job.setReducerClass(IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}
+	
 }
--- stackoverflow/src/main/java/br/edu/ifba/gsort/inf628/brazilianusers/Map.java
+++ stackoverflow/src/main/java/br/edu/ifba/gsort/inf628/brazilianusers/Map.java
@@ -5,14 +5,20 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import br.edu.ifba.gsort.inf628.util.MRUtil;
+
 public class Map extends Mapper<Object, Text, Text, Text> {
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		
-		String line = value.toString().toLowerCase();
+		java.util.Map<String, String> lineParsed = MRUtil.transformXmlToMap(value.toString());
+		String location = lineParsed.get("Location");
 		
-		if (line.contains("brazil") || line.contains("brasil")) {
-			context.write(new Text("Nome"), value);
+		// && (location.contains("brazil") || location.contains("brasil"))
+		if (location != null && (location.contains("rasil") || location.contains("razil"))) {
+			context.write(new Text(lineParsed.get("Id")), new Text(lineParsed.get("DisplayName") + " - " + location));
+		} else {
+			//context.write(new Text(lineParsed.get("Id")), new Text(location));
 		}
 		
 	}
